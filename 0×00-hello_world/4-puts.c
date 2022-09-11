9462374e2a02cf208736ae619c374fa012676bd2
Alfred Makura <111094240+alfred0496@users.noreply.github.com>
2022-09-11 21:17:30 +0200

    Create 4-puts.c

---
+++ 0×00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts "programming is like building a multingual puzzle \n");
+return (0)
+}
